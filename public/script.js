e577faa51227c70e8239352df02367bb624cc179
Arunesh Choudhary <nysdas@icloud.com>
2022-07-27 09:38:14 -0400

    Made custom Header and Footer (#25)

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// NAVIGATION
+// --------------------------------------------------------------------------------------------------------------------
+
+const btnNav = document.querySelector('.btn-mobile-nav');
+const headerNav = document.querySelector('.header');
+
+btnNav.addEventListener('click', function () {
+  headerNav.classList.toggle('nav-open');
+});
