2e909255e10d1a3967a2b9b8c7aee929c7aba38a
arunesh <nysdas@icloud.com>
2022-08-05 09:13:23 -0400

    add billing page

--- routes/viewsRoutes.js
+++ routes/viewsRoutes.js
@@ -7,5 +7,6 @@ router.get('/', viewsController.getOverview);
 router.get('/product/:slug', viewsController.getProduct);
 router.get('/login', viewsController.getLoginForm);
 router.get('/signup', viewsController.getSignUpForm);
+router.get('/billing', viewsController.getBillingPage);
 
 module.exports = router;
