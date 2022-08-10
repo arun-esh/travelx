69c9f10c3fd230c012c2ed7f26231b50c2cddcd2
arunesh <nysdas@icloud.com>
2022-08-10 11:53:54 -0400

    Login is working

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
-      url: 'http://localhost:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
