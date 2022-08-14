e8cef18227d5d11fe7fc67f9e7734d83bf5d4180
Arunesh Choudhary <nysdas@icloud.com>
2022-08-14 08:03:43 -0400

    multiple changes - send the username who is trying to login at console
    * add authController.login method to send outout to me
    
    * multiple changes - send the username who is trying to login at console
    
    * config.env
    changed the database login and URI
    File is added to gitignore from repo
    
    * controllers/viewsController.js
    We are now getting documents from original dataset
    Made sure we are retrieving what we exactly need
    Added multiple conditional to match the exception
    
    * models/productModel.js
    We are not using slug any more but _id
    Aldo commented out all slug related pre-middleware
    
    * views/base.pug
    Fixed favicon issue
    
    * views/overview.pug
    Instead of using name as slug, used _id from mongoose
    Some of the names had some non-URL-friendly characters

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -2,38 +2,67 @@ const Product = require(`./../models/productModel`);
 const catchAsync = require(`./../utils/catchAsync`);
 
 exports.getOverview = catchAsync(async (req, res) => {
-  // get the product from the database
-  const products = await Product.find({
-    bathrooms: { $gte: 1, $lte: 5 },
-    security_deposit: { $ne:  null },
-    security_deposit: { $gte: 1, $lte: 200 },
-    cleaning_fee: { $ne:  null },
-    cleaning_fee: { $gte: 0, $lte: 200 },
-    });
-
+  const products = await Product.find(
+    {
+      name : { $ne: ""},
+      summary: { $ne: "" },
+      description: { $ne: "" },
+      neighborhood_overview: { $ne: "" },
+      notes: { $ne: "" },
+      transit: { $ne: null },
+      access: { $ne: null },
+      house_rules: { $ne: null },
+      property_type: { $ne: null },
+      room_type: { $ne: null },
+      bed_type: { $ne: null },
+      bathrooms: { $ne: null },
+      bedrooms: { $ne: null },
+      beds: { $ne: null },
+      price: { $ne: null },
+      amenities: { $ne: null },
+      images: { $ne: null },
+      host: { $ne: null },
+      host_name: { $ne: null },
+      host_image: { $ne: null },
+      host_description: { $ne: null },
+      host_is_superhost: { $ne: null },
+      host_listings_count: { $ne: null },
+      host_total_listings_count: { $ne: null },
+      host_verifications: { $ne: null },
+      host_has_profile_pic: { $ne: null },
+      host_identity_verified: { $ne: null },
+      street: { $ne: null },
+      city: { $ne: null },
+      state: { $ne: null },
+      zipcode: { $ne: null },
+      country: { $ne: null },
+      latitude: { $ne: null },
+      longitude: { $ne: null },
+      is_location_exact: { $ne: null },
+      property_type: { $ne: null },
+      room_type: { $ne: null },
+      bed_type: { $ne: null },
+      security_deposit: { $ne:  null },
+      cleaning_fee: { $ne:  null },
+ 
+      // conditionals 
+      bathrooms: { $gte: 2, $lte: 5 },
+      security_deposit: { $gte: 1, $lte: 200 },
+      cleaning_fee: { $gte: 0, $lte: 200 },
+      }
+  );
   res.render('overview', {
     title: 'All Products',
-    products,
+    products
   });
 });
 
 exports.getProduct = catchAsync(async (req, res, next) => {
-  console.log("REQUIRED: " + req.params.slug);
-
-  // const product = await Product.findOne({ slug: req.params.slug })
-  
-  const product = await Product.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'review rating user'
-  });
-  console.log(product);
+  const product = await Product.findOne({ _id: req.params.slug });
   res.status(200).render('product', {
     title: 'Product',
     product 
-
   });
-
-  console.log(product);
 });
 
 exports.getLoginForm = (req, res) => {
