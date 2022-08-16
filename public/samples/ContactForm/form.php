a502d8f63b2eb6dbac69fec6f71a1082ef3c2a0b
HarshitOO7 <hbedi3@gmail.com>
2022-08-16 15:21:34 -0400

    added contact form

---
+++ public/samples/ContactForm/form.php
@@ -0,0 +1,39 @@
+<?php 
+if(isset($_POST['submit'])){
+    $to = "bedi8@uwindsor.ca"; 
+    $from = $_POST['email']; 
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    
+    header("Location:thank_you.html");
+    }
+?>
+
+<!DOCTYPE html>
+<!-- add css -->
+<head>
+<link rel="stylesheet" href="./../../css/style.css">
+<title>Form submission</title>
+</head>
+<body>
+
+<form action="form2.php" method="post">
+First Name: <input type="text" name="first_name"><br>
+Last Name: <input type="text" name="last_name"><br>
+Email: <input type="text" name="email"><br>
+Message:<br><textarea rows="5" name="message" cols="30"></textarea><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+
+</body>
+</html> 
