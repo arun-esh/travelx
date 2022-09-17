a962d6f022b51b167edf523a77bfa594730d1f17
Jalpa Patel <pjalpa015@gmail.com>
2022-09-17 13:49:36 -0400

    EDA update

--- ml/data_processing.py
+++ ml/data_processing.py
@@ -3,16 +3,16 @@ from os import listdir
 
 
 def load_data():
-    df = pd.read_csv('./ml/data.csv')
+    df = pd.read_csv('./ml/data/data.csv')
     return df
 
 def read_json():
-    df = pd.read_json('./ml/airbnb5000.json', lines=True, orient='records')
+    df = pd.read_json('./ml/data/airbnb5000.json', lines=True, orient='records')
     return df
 
 # convert json to csv
 def convert_csv(df):
-    df.to_csv('./ml/data.csv', index=False)
+    df.to_csv('./ml/data/data.csv', index=False)
 
 # list files in directory
 def list_files():
