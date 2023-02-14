b9d0a969ac8dbdc9e5a310225b00ad831ecae269
nysdas <nysdas@icloud.com>
2023-02-14 13:08:44 -0500

    netlify

--- app.js
+++ app.js
@@ -44,7 +44,8 @@ app.use((req, res, next) => {
 const productRouter = require(`./routes/productRoutes`);
 const userRouter = require(`./routes/userRoutes`);
 
-app.use('/', viewRouter);
+app.use('/.netlify/functions/server', viewRouter);
+// app.use('/', viewRouter);
 app.use('/api/v1/products', productRouter);
 app.use('/api/v1/users', userRouter);
 
