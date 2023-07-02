2ec445c2093bb12b444af7e3d68bca96f3b5f2bd
nysdas <nysdas@icloud.com>
2023-07-02 13:34:15 -0400

    add final

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+console.log(process.env.DATABASE);
+
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
   process.env.DATABASE_PASSWORD
